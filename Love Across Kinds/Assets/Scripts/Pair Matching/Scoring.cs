7c392b04516f973dc317f2ae9e32696826eb2863
Jonathanhong1999 <jonathanhong65@gmail.com>
2023-10-11 03:02:29 +0800

    pair matching

---
+++ Love Across Kinds/Assets/Scripts/Pair Matching/Scoring.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Scoring : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public int score = 0;
+    public int maxScore;
+    public int minScore;
+
+    void Start()
+    {
+        score = 0;
+    }
+
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+    }
+
+    public void RemoveScore(int newScore) 
+    {  
+        score -= newScore; 
+    }
+
+    public void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    void Update()
+    {
+        UpdateScore();
+
+        if(score >= maxScore)
+        {
+            score = maxScore;
+        }
+
+        if (score <= minScore)
+        {
+            score = minScore;
+        }
+    }
+}
