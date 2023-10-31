3831c78374e9fddd13061cd9615995206c33647a
Yejinhand69 <0134674@student.uow.edu.my>
2023-11-01 02:17:12 +0800

    transport point works for now ?

--- Love Across Kinds/Assets/Scripts/Camera/TransportPoint.cs
+++ Love Across Kinds/Assets/Scripts/Camera/TransportPoint.cs
@@ -6,13 +6,30 @@ using UnityEngine.SceneManagement;
 
 public class TransportPoint : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    public string sceneToLoad;
+    public GameObject TransportPanel;
+    //public string sceneToLoad;
     public Animator animator;
 
     public float delayBeforeLoad = 1.0f;
     private float pressTime;
     private bool isSwiping = false;
     public float swipeThreshold = 0.2f; // Adjust this threshold to your preference for distinguishing a tap from a swipe
+
+    public static TransportPoint instance;
+    private void Awake()
+    {
+        // Ensure only one instance of this script exists
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            //Destroy(gameObject);
+        }
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
@@ -31,10 +48,37 @@ public class TransportPoint : MonoBehaviour, IPointerDownHandler, IPointerUpHand
 
             if (pressDuration <= swipeThreshold)
             {
-                animator.SetTrigger("FadeOut");
-                StartCoroutine(LoadSceneWithDelay(sceneToLoad, delayBeforeLoad));
+                OpenTransportPanel();
+                //animator.SetTrigger("FadeOut");
+                //StartCoroutine(LoadSceneWithDelay(sceneToLoad, delayBeforeLoad));
             }
         }
+    }
+    public void OpenTransportPanel()
+    {
+        if (TransportPanel != null)
+        {
+
+            Animator animator = TransportPanel.GetComponent<Animator>();
+            if (animator != null)
+            {
+                //AudioManager.instance.PlaySFX("Button Press");
+
+                bool isOpen = animator.GetBool("openTransportPanel");
+
+                animator.SetBool("openTransportPanel", !isOpen);
+            }
+
+        }
+    }
+
+    public void LoadLobby()
+    {
+        SceneManager.LoadScene("Lobby0");
+    }
+    public void LoadRecording()
+    {
+        SceneManager.LoadScene("Recording0");
     }
 
     private IEnumerator LoadSceneWithDelay(string sceneName, float delay)
