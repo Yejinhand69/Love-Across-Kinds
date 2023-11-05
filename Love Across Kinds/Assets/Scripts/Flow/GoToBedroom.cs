a14587ee28f0a2c961e42fa29d3d2f8a463bc57b
Yejinhand69 <0134674@student.uow.edu.my>
2023-11-06 00:20:03 +0800

    Go to bedroom

--- Love Across Kinds/Assets/Scripts/Flow/GoToBedroom.cs
+++ Love Across Kinds/Assets/Scripts/Flow/GoToBedroom.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class Bed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class GoToBedroom : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
-    public GameObject sleepPanel;
+    //public GameObject sleepPanel;
     private float pressTime;
     private bool isSwiping = false;
     public float swipeThreshold = 0.2f; // Adjust this threshold to your preference for distinguishing a tap from a swipe
@@ -29,33 +29,34 @@ public class Bed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
             if (pressDuration <= swipeThreshold)
             {
-                OpenSleepPanel();
+                Camera.main.transform.position = new Vector3(0.0f, 4.33f, -7.06f);
+                //OpenSleepPanel();
                 //animator.SetTrigger("FadeOut");
                 //StartCoroutine(LoadSceneWithDelay(sceneToLoad, delayBeforeLoad));
             }
         }
     }
-    public void OpenSleepPanel()
-    {
-        if (sleepPanel != null)
-        {
+    //public void OpenSleepPanel()
+    //{
+    //    if (sleepPanel != null)
+    //    {
 
-            Animator animator = sleepPanel.GetComponent<Animator>();
-            if (animator != null)
-            {
-                //AudioManager.instance.PlaySFX("Button Press");
+    //        Animator animator = sleepPanel.GetComponent<Animator>();
+    //        if (animator != null)
+    //        {
+    //            //AudioManager.instance.PlaySFX("Button Press");
 
-                bool isOpen = animator.GetBool("openSleepPanel");
+    //            bool isOpen = animator.GetBool("openSleepPanel");
 
-                animator.SetBool("openSleepPanel", !isOpen);
-            }
+    //            animator.SetBool("openSleepPanel", !isOpen);
+    //        }
 
-        }
-    }
+    //    }
+    //}
 
-    public void LoadLobby1()//episode 0 change to episode 1
-    {
-        SceneManager.LoadScene("Lobby1");
-    }
+    //public void LoadLobby1()//episode 0 change to episode 1
+    //{
+    //    SceneManager.LoadScene("Lobby1");
+    //}
     
 }
