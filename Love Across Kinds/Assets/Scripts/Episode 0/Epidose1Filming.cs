2cd6cbf9e3aaf97beac27c1d6636ac18eec550f7
CallMeZhiJian <zjiandev@gmail.com>
2023-11-07 01:59:46 +0800

    Episode1Filming script adjustment - switch case indicating where to play SFX

--- Love Across Kinds/Assets/Scripts/Episode 0/Epidose1Filming.cs
+++ Love Across Kinds/Assets/Scripts/Episode 0/Epidose1Filming.cs
@@ -42,6 +42,63 @@ public class Epidose1Filming : MonoBehaviour
             case 18:
                 //SFX - Light off
                 break;
+            case 25:
+                //SFX - Light
+                break;
+            case 28:
+                //SFX - Crowd cheer and clap
+                break;
+            case 32:
+                //SFX - Light off
+                break;
+            case 38:
+                //SFX - Light on
+                break;
+            case 42:
+                //SFX - Clap
+                break;
+            case 43:
+                //SFX - Light off
+                break;
+            case 48:
+                //SFX - Light on
+                break;
+            case 49:
+                //SFX - Cheer and Clap
+                break;
+            case 51:
+                //SFX - Light off
+                break;
+            case 55:
+                //SFX - Light on
+                break;
+            case 56:
+                //SFX - Cheer and Clap
+                break;
+            case 58:
+                //SFX - Cheer and Clap
+                break;
+            case 59:
+                //SFX - Light off
+                break;
+            case 62:
+                //SFX - Light on
+                break;
+            case 70:
+                //SFX - Crowd oooooo
+                break;
+            case 73:
+                //SFX - Light on
+                break;
+            case 75:
+                //SFX - Light on
+                break;
+            case 77:
+                //SFX - Light off
+                break;
+            case 84:
+                //SFX - Light off
+                break;
         }
     }
 }
