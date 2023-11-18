406af5a8b16832eeda4d1258da0c8812bbd69aba
Yejinhand69 <0134674@student.uow.edu.my>
2023-11-19 00:00:43 +0800

    sprite billboard script minor changes

--- Love Across Kinds/Assets/Scripts/Camera/SpriteBillboard.cs
+++ Love Across Kinds/Assets/Scripts/Camera/SpriteBillboard.cs
@@ -8,6 +8,9 @@ public class SpriteBillboard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y , 0f);
+        if (Camera.main != null && Camera.main.isActiveAndEnabled)
+        {
+            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        }
     }
 }
