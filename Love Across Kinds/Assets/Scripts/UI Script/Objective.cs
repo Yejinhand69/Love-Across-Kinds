62bdf23926233fa5d33a2bf124cb3c922be2fde4
Jonathanhong1999 <jonathanhong65@gmail.com>
2023-11-29 01:33:09 +0800

    objective1

--- Love Across Kinds/Assets/Scripts/UI Script/Objective.cs
+++ Love Across Kinds/Assets/Scripts/UI Script/Objective.cs
@@ -7,30 +7,41 @@ public class Objective : MonoBehaviour
 {
     public string[] task;
     public string[] title;
+
+    public GameObject[] prologue;
     //public ObjectiveTitle T;
 
     private TextMeshProUGUI objectives;
     void Start()
     {
         objectives = GetComponent<TextMeshProUGUI>();
+
         
-        for( int i = 0; i < title.Length; i++ )
-        {
-            title[i] = "<b>" + title[i] + "</b>";
-        }
         //objectives.fontStyle = FontStyles.Bold;
     }
 
     void Update()
     {
-        if(PhaseManager.instance.currentPhase == "Prologue")
+        for (int i = 0; i < title.Length; i++)
+        {                                    // title                                                 // task
+            title[i] = "<size={4 + i * 5}><line-height=70%>" + "<b>" + title[i] + "</b>" + "</size><line-height=75%>";
+        }
+
+        if (PhaseManager.instance.currentPhase == "Prologue")
         {
-            objectives.text = title[0] + "\n" + task[0] + "\n" + task[1] + "\n" + task[2] + "\n" + "\n" + title[1] + "\n" + task[3];            
+            objectives.text = title[0] + "\n" + "\n" + task[0] + "\n" + task[1] + "\n" + task[2] + "\n" + "\n" + "\n" + "\n" + title[1] + "\n" + "\n" + task[3];            
         }
 
         if (PhaseManager.instance.currentPhase == "PreProduction")
         {
-            objectives.text = title[3] + "\n" + task[5] + "\n" + task[6] + "\n" + task[7] + "\n" + "\n" + title[4] + "\n" + task[8] + "\n" + "\n" + title[5] + "\n" + task[9];       
+            objectives.text = title[3] + "\n" + task[5] + "\n" + task[6] + "\n" + task[7] + "\n" + "\n" + title[4] + "\n" + task[8] + "\n" + "\n" + title[5] + "\n" + task[9];
+
+            for (int i = 0;i <= prologue.Length;i++)
+            {
+                prologue[i].SetActive(false);
+            }
+            
+                  
         }
 
         if (PhaseManager.instance.currentPhase == "Filming")
