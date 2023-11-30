8f86437e8de34d430e7634e61740bf77493166c4
CallMeZhiJian <zjiandev@gmail.com>
2023-11-30 15:40:55 +0800

    Phase Manager update

--- Love Across Kinds/Assets/Scripts/Flow/PhaseManager.cs
+++ Love Across Kinds/Assets/Scripts/Flow/PhaseManager.cs
@@ -16,7 +16,7 @@ public class PhaseManager : MonoBehaviour
     public static PhaseManager instance;
 
     private DialogueTrigger dialogueTrigger;
-    private PhoneOpener phoneOpener;
+    //private PhoneOpener phoneOpener;
 
     public bool isDonePrologue;
     public bool isDonePP1;
@@ -58,6 +58,7 @@ public class PhaseManager : MonoBehaviour
             AudioManager.instance.PlaySFX("Notification 2");
 
             dialogueTrigger.StartDialogue(" ", 0);
+            instance.isDonePP1 = true;
             Debug.Log("Dialogue Start PP");
         }
 
@@ -69,6 +70,7 @@ public class PhaseManager : MonoBehaviour
             AudioManager.instance.PlaySFX("Notification 2");
 
             dialogueTrigger.StartDialogue(" ", 0);
+            instance.isDoneS1 = true;
             Debug.Log("Dialogue Start S");
         }
     }
@@ -87,27 +89,27 @@ public class PhaseManager : MonoBehaviour
             instance.isDonePrologue = true;
         }
 
-        if (currentPhase == "PreProduction" && currentEpisode == 1 && !instance.isDonePP1)
-        {
-            while (DialogueManager.dialogueActive)
-            {
-                continue;
-            }
+        //if (currentPhase == "PreProduction" && currentEpisode == 1 && !instance.isDonePP1)
+        //{
+        //    while (DialogueManager.dialogueActive)
+        //    {
+        //        continue;
+        //    }
             
-            StartCoroutine(ShowPhoneMessage());
-            instance.isDonePP1 = true;
-        }
+        //    StartCoroutine(ShowPhoneMessage());
+            
+        //}
 
-        if(currentPhase == "Special" && currentEpisode == 1 && !instance.isDoneS1)
-        {
-            while (DialogueManager.dialogueActive)
-            {
-                continue;
-            }
+        //if(currentPhase == "Special" && currentEpisode == 1 && !instance.isDoneS1)
+        //{
+        //    while (DialogueManager.dialogueActive)
+        //    {
+        //        continue;
+        //    }
 
-            StartCoroutine(ShowPhoneMessage());
-            instance.isDoneS1 =true;
-        }
+        //    StartCoroutine(ShowPhoneMessage());
+            
+        //}
     }
 
     private void UpdatePhaseText()
@@ -148,30 +150,30 @@ public class PhaseManager : MonoBehaviour
         dialogueTrigger.StartDialogue(" ", 69);
     }
 
-    IEnumerator ShowPhoneMessage()
-    {
-        yield return new WaitForSeconds(0.5f);
-        while (DialogueManager.dialogueActive)
-        {
-            yield return null;
-        }
+    //IEnumerator ShowPhoneMessage()
+    //{
+    //    yield return new WaitForSeconds(0.5f);
+    //    while (DialogueManager.dialogueActive)
+    //    {
+    //        yield return null;
+    //    }
 
-        //phoneOpener.OpenPhone();
+    //    //phoneOpener.OpenPhone();
 
-        yield return new WaitForSeconds(0.5f);
+    //    yield return new WaitForSeconds(0.5f);
         
-        //phoneOpener.OpenMessagePanel();
+    //    //phoneOpener.OpenMessagePanel();
 
-        //while (phoneOpener.PhonePanel.GetComponent<Animator>().GetBool("openPhone"))
-        //{
-        //    yield return null;
-        //}
+    //    //while (phoneOpener.PhonePanel.GetComponent<Animator>().GetBool("openPhone"))
+    //    //{
+    //    //    yield return null;
+    //    //}
 
-        //if (phoneOpener.PhonePanel.GetComponent<Animator>().GetBool("openPhone") == false)
-        //{
-        //    //phoneOpener.OpenMessagePanel();
-        //}
+    //    //if (phoneOpener.PhonePanel.GetComponent<Animator>().GetBool("openPhone") == false)
+    //    //{
+    //    //    //phoneOpener.OpenMessagePanel();
+    //    //}
 
-        dialogueTrigger.StartDialogue(" ", 1);
-    }
+    //    //dialogueTrigger.StartDialogue(" ", 1);
+    //}
 }
