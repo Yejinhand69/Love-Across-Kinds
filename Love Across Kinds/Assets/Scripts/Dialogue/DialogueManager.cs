8bd385145d4b01fce564771ad60e3c37615981a9
CallMeZhiJian <zjiandev@gmail.com>
2023-11-30 15:19:03 +0800

    - DialogueData update - Script update

--- Love Across Kinds/Assets/Scripts/Dialogue/DialogueManager.cs
+++ Love Across Kinds/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -243,7 +243,6 @@ public class DialogueManager : MonoBehaviour
                     switch (datas[currIndexPos].expression)
                     {
                         case 1:
-                            Debug.Log("Happy");
                             //Expression change here...
                             characterAnim.SetBool("isSmile", true);
                             characterAnim.SetBool("isAngry", false);
@@ -251,7 +250,6 @@ public class DialogueManager : MonoBehaviour
                             characterAnim.SetBool("isShy", false);
                             break;
                         case 2:
-                            Debug.Log("Angry");
                             //Expression change here...
                             characterAnim.SetBool("isSmile", false);
                             characterAnim.SetBool("isAngry", true);
@@ -259,7 +257,6 @@ public class DialogueManager : MonoBehaviour
                             characterAnim.SetBool("isShy", false);
                             break;
                         case 3:
-                            Debug.Log("Sad");
                             //Expression change here...
                             characterAnim.SetBool("isSmile", false);
                             characterAnim.SetBool("isAngry", false);
@@ -267,7 +264,6 @@ public class DialogueManager : MonoBehaviour
                             characterAnim.SetBool("isShy", false);
                             break;
                         case 4:
-                            Debug.Log("Shy");
                             //Expression change here...
                             characterAnim.SetBool("isSmile", false);
                             characterAnim.SetBool("isAngry", false);
@@ -275,7 +271,6 @@ public class DialogueManager : MonoBehaviour
                             characterAnim.SetBool("isShy", true);
                             break;
                         default:
-                            Debug.Log("Neutral");
                             //Expression change here...
                             characterAnim.SetBool("isSmile", false);
                             characterAnim.SetBool("isAngry", false);
